a8a3a9ec4c5e45303413c03c3e155793d1adb79d
Eddie Torrejon <eddie@torrejon.ca>
2014-11-19 22:57:12 -0500

    Grid can now be drawn with arbitrary width and height.
    todo: find a good JS testing framework and runner.

--- game.of.life.js
+++ game.of.life.js
@@ -1,62 +1,79 @@
 var CANVAS_ELEMENT_ID = 'game_of_life';
 var CELL_SIZE_IN_PIXELS = 10;
+var GRID_WIDTH_IN_CELLS = 80;
+var GRID_HEIGHT_IN_CELLS = 80;
+
+var _context;
+var _cells = [];
 
 function startSimulation() {
-  var context = createContext(CANVAS_ELEMENT_ID);
-  drawBoard(context);
+  initializeSimulation(); 
+  var seed = [ {x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2} ];
+  populateCells(seed);
 }
 
-
-function drawBoard(context) {
-  drawGrid(context);
+function initializeSimulation(gridSize) {
+  initializeCanvas();
+  drawGrid();
 }
 
-function populateCell(context, position) {
-  context.fillRect(position.x, position.y, CELL_SIZE_IN_PIXELS, CELL_SIZE_IN_PIXELS);
+function initializeCanvas() {
+  var canvas = document.getElementById(CANVAS_ELEMENT_ID);
+  canvas.width = GRID_WIDTH_IN_CELLS * CELL_SIZE_IN_PIXELS;
+  canvas.height = GRID_HEIGHT_IN_CELLS * CELL_SIZE_IN_PIXELS;
+
+  _context = canvas.getContext('2d');
+  _context.translate(0.5, 0.5); // shifts lines half a pixel - to avoid blurry lines
+  _context.strokeStyle = "#eee";
 }
 
-function drawGrid(context) {
-  var gridWidth = context.canvas.width;
-  var gridHeight = context.canvas.height;
-  var numberOfGridLines = gridWidth / CELL_SIZE_IN_PIXELS;
+function drawGrid() {
+  var gridWidth = GRID_WIDTH_IN_CELLS * CELL_SIZE_IN_PIXELS;
+  var gridHeight = GRID_HEIGHT_IN_CELLS * CELL_SIZE_IN_PIXELS;
 
-  for(var i = 0; i < numberOfGridLines; i++) {
+  for(var i = 0; i < GRID_HEIGHT_IN_CELLS; i++) {
     var start_offset = CELL_SIZE_IN_PIXELS * i;
-    log("start_offset: {offset}".supplant({ offset: start_offset }));
-
     var verticalStartPoint = { x: start_offset, y: 0 };
     var verticalEndPoint = { x: start_offset, y: gridHeight };
+    drawLine(verticalStartPoint, verticalEndPoint);
+  }
 
+  for(var i = 0; i < GRID_WIDTH_IN_CELLS; i++) {
+    var start_offset = CELL_SIZE_IN_PIXELS * i;
     var horizontalStartPoint = { x: 0, y: start_offset };
     var horizontalEndPoint = { x: gridWidth, y: start_offset };
-
-    drawLine(context, verticalStartPoint, verticalEndPoint);
-    drawLine(context, horizontalStartPoint, horizontalEndPoint);
+    drawLine(horizontalStartPoint, horizontalEndPoint);
   }
 }
 
-function createContext(canvasElementId) {
-  var canvas = document.getElementById(canvasElementId)
-  var context = canvas.getContext('2d');
-  context.translate(0.5, 0.5); // shifts lines half a pixel - to avoid blurry lines
-  context.strokeStyle = "#eee";
-  return context;
+function populateCells(cellPositions) {
+  var i;
+  for(i = 0; i < cellPositions.length; i++) {
+    populateCell(cellPositions[i]);
+  }
 }
+
+function populateCell(position) {
+  _cells[_cells.length] = position;
+  var xCoordinate = position.x * CELL_SIZE_IN_PIXELS;
+  var yCoordinate = position.y * CELL_SIZE_IN_PIXELS;
+  _context.fillRect(xCoordinate, yCoordinate, CELL_SIZE_IN_PIXELS, CELL_SIZE_IN_PIXELS);
+}
+
 
 function log(message) {
   $("#log").prepend( "<p>{m}</p>".supplant({m: message}) ); 
 }
 
-function drawLine(context, startPoint, endPoint) {
+function drawLine(startPoint, endPoint) {
   log("drawing line from ({x1}, {y1}) to ({x2}, {y2})".supplant({ x1: startPoint.x + 0.5, 
                                                                   y1: startPoint.y + 0.5, 
                                                                   x2: endPoint.x + 0.5, 
                                                                   y2: endPoint.y + 0.5}));
 
-  context.beginPath();
-  context.moveTo(startPoint.x, startPoint.y);
-  context.lineTo(endPoint.x, endPoint.y);
-  context.closePath();
-  context.stroke();
+  _context.beginPath();
+  _context.moveTo(startPoint.x, startPoint.y);
+  _context.lineTo(endPoint.x, endPoint.y);
+  _context.closePath();
+  _context.stroke();
 }
-
--- utilities.js
+++ utilities.js
@@ -12,6 +12,22 @@ if (!String.prototype.supplant) {
     };
 }
 
+// matrix function taken from Douglas Crockford's "Javascript: The Good Parts"
+// http://shop.oreilly.com/product/9780596517748.do
+if (!Array.matrix) {
+  Array.matrix = function(m, n, initial) {
+    var a, i, j, mat = [];
+    for(i = 0; i < m; i += 1) {
+      a = [];
+      for(j = 0; j < n; j += 1) {
+        a[j] = initial;
+      }
+      mat[i] = a;
+    }
+    return mat;
+  }
+}
+
 if (!Math.toRadians) {
   Math.toRadians = function (deg) {
     return deg * Math.PI / 180;
